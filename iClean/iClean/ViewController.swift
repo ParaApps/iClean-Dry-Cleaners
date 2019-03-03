25c342e08c52f0e18b8af982fb39159f8fd34d0d
ParaApps <paradonp@outlook.com>
2019-03-03 21:29:08 +0800

    adding iClean folder with all its contents

---
+++ iClean/iClean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iClean
+//
+//  Created by Paradon Panjaroen on 30/12/18.
+//  Copyright © 2018 iClean Dry Cleaners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
